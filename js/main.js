388770538727c7b91aa7721112ae108a6319bfc3
Suresh Kumar <mail.sk1999@gmail.com>
2018-07-07 09:35:31 +0530

    Fixes #1

--- js/main.js
+++ js/main.js
@@ -1,23 +1,41 @@
 function makeCompleted(element){
     let parent = element.parentNode;
+    let flag = localStorage.getItem('completed');
+    flag = flag.split(',');
+    
+    
     if(element.checked) {
-        parent.style.textDecoration = 'line-through';
+        
+        flag[parent.getAttribute('data-index')] = "1";
     }
     else{
-        parent.style.textDecoration = 'none';
+        
+        flag[parent.getAttribute('data-index')] = "0";
     }
+    localStorage.setItem('completed', flag.join(','))
+    update();
 }
 
 function update(){
+    
     var task = document.getElementById('tasks');
 
     var c = localStorage.getItem("tasks");
     let final = '';
+    let status = localStorage.getItem('completed');
+
     if(c){
         var previous = c.split(',');
-        previous.forEach(function(value){
+        let b = status.split(',');
+        previous.forEach(function(value, index){
+            
             if(value)
-            final = final + `<li data-task=${value} ><input type="checkbox" onClick="makeCompleted(this)">` + value + '<span class="delete" onClick="remove(this)"><img src="./delete-16.png" /></span></li>';
+                if(b[index] === "0"){
+                    final = final + `<li data-task=${value} data-index=${index}><input type="checkbox" onClick="makeCompleted(this)" >` + value + '<span class="delete" onClick="remove(this)"><img src="./delete-16.png" /></span></li>';
+                }
+                else
+                final = final + `<li data-task=${value} data-index=${index}  style="text-decoration:line-through"><input type="checkbox" onClick="makeCompleted(this)" checked="true">` + value + '<span class="delete" onClick="remove(this)" ><img src="./delete-16.png" /></span></li>';
+            
         })
         
     }
@@ -27,6 +45,7 @@ function update(){
 
 function resetItems(){
     localStorage.removeItem("tasks");
+    localStorage.removeItem('completed');
     update();
 }
 function addItem(){
@@ -40,10 +59,20 @@ function addItem(){
         } else{
             temp = text;
         }
+        let previous = localStorage.getItem('completed');
+        let cFlag = [];
+        if (previous !=null){
+            cFlag = previous.split(",");
+            
+        }
+        cFlag.push("0");
+
         localStorage.setItem("tasks",temp);
         
+        localStorage.setItem('completed',cFlag.join(','));
         update();
     }
+    
 }
 
 function getEvent(event){
@@ -55,12 +84,18 @@ function remove(element){
     var parent = element.parentNode;
     var tasks = localStorage.getItem('tasks').split(',');
     var remove = parent.dataset.task;
-    tasks.splice(tasks.indexOf(remove),1);
-    console.log(tasks);
+    let index = tasks.indexOf(remove)
+    tasks.pop(remove);
     var final = '';
+    var c = localStorage.getItem('completed').split(',');
+
+    if(index>-1)
+        c.splice(index,1);
     tasks.forEach(function (value) { 
-        final += `,${value}`;
+        if(value!=='')
+        final += `${value},`;
      })
-     localStorage.setItem('tasks', final);
+     localStorage.setItem('tasks', final.slice(0,final.length-1));
+     localStorage.setItem('completed',c.join(','));
      update();
 }
